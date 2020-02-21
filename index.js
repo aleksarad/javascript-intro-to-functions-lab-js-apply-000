828413ba129967158fda6aff19b1038d1ba68d78
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-02-21 21:34:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 
 
-//   sayHiToGrandma(string)
-//     5) returns "I can't hear you!" if `string` is lowercase
-//     6) returns "YES INDEED!" if `string` is uppercase
-//     7) returns "I love you, too." if `string` is "I love you, Grandma."`
-
 function sayHiToGrandma(str){
   if(str === str.toLowerCase()) {
     return `I can't hear you!`;
