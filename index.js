f230f7e80f9d5519785e9c3348b550f512a1a6e8
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-02-21 21:25:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,9 +13,7 @@
 //     6) returns "YES INDEED!" if `string` is uppercase
 //     7) returns "I love you, too." if `string` is "I love you, Grandma."`
     
-    var shout = (string) => {
-      return str.toUpperCase();
-    }
+  function shout()
     
     var whisper = (string) => { return str.toLowerCase(); 
     }
