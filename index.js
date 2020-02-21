36b90a8b07b60ff2225338112d8e4f76040c24a2
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-02-21 21:29:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 
 
-//   logShout(string)
-//     3) calls console.log() its one argument in all caps
-
-//   logWhisper(string)
-//     4) calls console.log() its one argument in all lowercase
-
 //   sayHiToGrandma(string)
 //     5) returns "I can't hear you!" if `string` is lowercase
 //     6) returns "YES INDEED!" if `string` is uppercase
 //     7) returns "I love you, too." if `string` is "I love you, Grandma."`
+
+function
 
   // reguluar
   function shout(string) {
@@ -23,4 +19,8 @@
   
   function logShout(str) {
     console.log(str.toUpperCase());
+  }
+  
+  function logWhisper(str) {
+    console.log(str.toLowerCase());
   }
