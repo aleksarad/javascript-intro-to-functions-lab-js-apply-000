b50d41f62605ca9197b4acdf75aebbd00a32dde0
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-02-21 21:24:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-  shout(string)
-    1) receives one argument and returns it in all caps
 
-  whisper(string)
-    2) receives one argument and returns it in all lowercase
+// whisper(string)
+//     2) receives one argument and returns it in all lowercase
 
-  logShout(string)
-    3) calls console.log() its one argument in all caps
+//   logShout(string)
+//     3) calls console.log() its one argument in all caps
 
-  logWhisper(string)
-    4) calls console.log() its one argument in all lowercase
+//   logWhisper(string)
+//     4) calls console.log() its one argument in all lowercase
 
-  sayHiToGrandma(string)
-    5) returns "I can't hear you!" if `string` is lowercase
-    6) returns "YES INDEED!" if `string` is uppercase
-    7) returns "I love you, too." if `string` is "I love you, Grandma."`
+//   sayHiToGrandma(string)
+//     5) returns "I can't hear you!" if `string` is lowercase
+//     6) returns "YES INDEED!" if `string` is uppercase
+//     7) returns "I love you, too." if `string` is "I love you, Grandma."`
     
-    var shout = (string) = > {
+    var shout = (string) => {
       return str.toUpperCase();
+    }
+    
+    var whisper = (string) => { return str.toLowerCase(); 
     }
